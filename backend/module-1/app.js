31b77971b1071b2c47ce2ec0e1d9ae9840ab1a9c
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-05-31 17:16:12 +0530

    request routing in node js and conditional routing in nodejs

--- backend/module-1/app.js
+++ backend/module-1/app.js
@@ -2,9 +2,29 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
+
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>HomePage</title></head>");
+    res.write("<body>");
+    res.write("<h1>HomePage</h1>");
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
+  } else if (req.url === "/users") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Users</title></head>");
+    res.write("<body>");
+    res.write("<h1>Users</h1>");
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
-  req.write("<head><title>Node js</title></head>");
+  res.write("<head><title>Node js</title></head>");
   res.write("<body>");
   res.write("<h1>hello world</h1>");
   res.write("</body>");
