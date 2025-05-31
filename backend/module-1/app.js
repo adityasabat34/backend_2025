d494b79c45bf2bc2503e0b5c068e37be06aaea82
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-05-31 16:59:30 +0530

    request and response header in node js

--- backend/module-1/app.js
+++ backend/module-1/app.js
@@ -1,7 +1,17 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  req.write("<head><title>Node js</title></head>");
+  res.write("<body>");
+  res.write("<h1>hello world</h1>");
+  res.write("</body>");
+  res.write("</html>");
+  res.end();
 });
-
-server.listen(5500);
+const PORT = 5500;
+server.listen(PORT, () => {
+  console.log(`server is running on http://localhost:${PORT}`);
+});
