8ffdfcec59ac87280d41063d3a2dea559ea3ef37
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-06-02 16:30:09 +0530

    user input in node js form handling

--- backend/module-1/app.js
+++ backend/module-1/app.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
@@ -6,20 +7,24 @@ const server = http.createServer((req, res) => {
   if (req.url === "/") {
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
-    res.write("<head><title>HomePage</title></head>");
+    res.write("<head><title>Users</title></head>");
     res.write("<body>");
-    res.write("<h1>HomePage</h1>");
+    res.write("<h1>Enter Your Details</h1>");
+    res.write("<form action='/create-user' method='POST'>");
+    res.write("<input type='text' name='username' placeholder='username'/>");
+    res.write("<label for='male'>male</label>");
+    res.write("<input type='radio' name='gender' id='male' vlaue='male'/>");
+    res.write("<label for='female'>Female</label>");
+    res.write("<input type='radio' name='gender' id='female' value='female'/>");
+    res.write("<input type='submit' value='submit'/>");
+    res.write("</form>");
     res.write("</body>");
     res.write("</html>");
     return res.end();
-  } else if (req.url === "/users") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>Users</title></head>");
-    res.write("<body>");
-    res.write("<h1>Users</h1>");
-    res.write("</body>");
-    res.write("</html>");
+  } else if (req.url.toLowerCase() == "/create-user" && req.method == "POST") {
+    fs.writeFileSync("./users.txt", "hello");
+    res.statusCode = 302;
+    res.setHeader("Location", "/hhs");
     return res.end();
   }
   res.setHeader("Content-Type", "text/html");
