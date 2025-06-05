4a9fa71ba236ba6431dbff8fff02af21fd2105c6
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-06-05 16:29:04 +0530

    error fixed in user input thorugh node js

--- backend/module-1/user.js
+++ backend/module-1/user.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  console.log(req.url, req.method);
 
   if (req.url === "/") {
     res.setHeader("Content-Type", "text/html");
@@ -31,8 +31,12 @@ const server = http.createServer((req, res) => {
     req.on("data", (chunk) => {
       console.log(chunk);
     });
-    
-    res.setHeader("Content-Type", "text/html");
+    fs.writeFileSync("user.txt", "Prashant Jain");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>Complete Coding</title></head>");
   res.write("<body><h1>Like / Share / Subscribe</h1></body>");
