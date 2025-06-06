51b783144a22202e014c0f2790659b37736e7ee1
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-06-06 21:05:58 +0530

    now displaying result in another page

--- backend/module-2/sum.js
+++ backend/module-2/sum.js
@@ -12,8 +12,22 @@ const sumOfNumRequest = (req, res) => {
     const bodyObj = Object.fromEntries(params);
     const result = Number(bodyObj.num1) + Number(bodyObj.num2);
     console.log(result);
-    res.end();
+
+    res.setHeader("Content-Type", "text/html");
+    res.write(`
+        <html>
+  <head>
+    <title>Document</title>
+  </head>
+  <body>
+      <h1>Here is your result ${result}</h1>
+     
+  </body>
+</html>
+        `);
   });
+
+  return res.end();
 };
 
 module.exports = sumOfNumRequest;
