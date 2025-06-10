3287fd9494d90bc1b936786b620204e78ac7a07b
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-06-10 17:28:07 +0530

    run async and sync task

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,21 @@
-console.log("hello");
-
 const fs = require("fs");
-fs.writeFile("./hello.txt", "hello Nodejs", (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("success");
-  }
+
+console.log("1. Start of script");
+// Synchronous (blocking) operation
+console.log("2. Reading file synchronously");
+const dataSync = fs.readFileSync("user-details.txt", "utf8");
+console.log("3. Synchronous read complete");
+// Asynchronous (non-blocking) operation
+console.log("4. Reading file asynchronously");
+fs.readFile("user-details.txt", "utf8", (err, dataAsync) => {
+  if (err) throw err;
+  console.log("6. Asynchronous read complete");
 });
+console.log("5. End of script");
+
+//  1
+//  6
+//  2
+//  4
+//  3
+//  5
