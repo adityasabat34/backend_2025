3c92cd2d98f123702f6f8ff54f064361911ef9f8
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-06-16 23:59:35 +0530

    testing of error and server

--- backend/module-4/app.js
+++ backend/module-4/app.js
@@ -1,4 +1,6 @@
 const http = require("http");
+const fs = require("fs");
+const test = require("./test");
 
 PORT = 4001;
 
@@ -6,6 +8,7 @@ const server = http.createServer((req, res) => {
   console.log(req.url);
   res.setHeader("Content-Type", "text/html");
   res.write("<h1>Hello Server</h1>");
+  test();
 });
 
 server.listen(PORT, (req, res) => {
---
+++ backend/module-4/test.js
@@ -0,0 +1,5 @@
+const test = (req, res) => {
+  console.log("Me aagya bhai");
+};
+
+module.exports = test;
