5e85d79f5994a3fd1f564a85e4ad482084c8ae9e
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-07-28 17:31:35 +0530

    middleware in post and get and use methods

--- backend/module-4/app.js
+++ backend/module-4/app.js
@@ -1,25 +1,48 @@
-const http = require("http");
 const express = require("express");
 
 const app = express();
 
-const server = http.createServer(app);
-
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
   console.log("first middleware", req.url, req.method);
   next();
 });
 
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
   console.log("second middleware", req.url, req.method);
   next();
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use("/third", (req, res, next) => {
+  // res.send("third middleware");
+  console.log("third middleware", req.url, req.method);
+  next();
 });
 
+// app.use("/", (req, res, next) => {
+//   console.log("Handling / for get request", req.url, req.method);
+//   res.send("Handling / for get request");
+//   // next();
+// });
+
+app.get("/contact-us", (req, res, next) => {
+  console.log("Handling /contact-us for get request", req.url, req.method);
+  res.send(
+    `<h1>Please give your details here</h1>
+    <form action="/contact-us" method="POST">
+      <input type="text" name="name" placeholder="Enter your name" />
+      <input type="email" name="email" placeholder="Enter your Email" />
+      <input type="Submit" />
+    </form>
+    `
+  );
+  // next();
+});
+
+app.post("/contact-us", (req, res, next) => {
+  console.log("Handling /contact-us for post request", req.url, req.method);
+  res.send("Handling /contact-us for post request");
+});
 const PORT = 5000;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on address http://localhost:${PORT}`);
 });
