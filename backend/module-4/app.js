cb57ed0a4bc4368d7c849aca28b9ec3f7cd91020
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-10-31 13:18:29 +0530

    body parser uses learnt

--- backend/module-4/app.js
+++ backend/module-4/app.js
@@ -26,22 +26,40 @@ app.use("/third", (req, res, next) => {
 
 app.get("/contact-us", (req, res, next) => {
   console.log("Handling /contact-us for get request", req.url, req.method);
-  res.send(
-    `<h1>Please give your details here</h1>
-    <form action="/contact-us" method="POST">
+  res.send(`<h1>Please give your details here</h1>
+    <form method="POST" action="/contact-us">
       <input type="text" name="name" placeholder="Enter your name" />
-      <input type="email" name="email" placeholder="Enter your Email" />
-      <input type="Submit" />
+      <input type="email" name="email" placeholder="Enter your email" />
+      <button type="submit">Submit</button>
     </form>
-    `
-  );
-  // next();
+  `);
+  next();
 });
 
 app.post("/contact-us", (req, res, next) => {
-  console.log("Handling /contact-us for post request", req.url, req.method);
+  console.log(
+    "Handling /contact-us for post request",
+    req.url,
+    req.method,
+    req.body
+  );
+  next();
+  // res.send("Handling /contact-us for post request");
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/contact-us", (req, res, next) => {
+  console.log(
+    "Handling /contact-us for post request",
+    req.url,
+    req.method,
+    req.body
+  );
   res.send("Handling /contact-us for post request");
 });
+
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on address http://localhost:${PORT}`);
