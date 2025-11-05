c5219d120269d9134f847334777df42d05486426
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-11-05 19:45:02 +0530

    raw routing to express routing

--- backend/module-4/routes/hostRoutes.js
+++ backend/module-4/routes/hostRoutes.js
@@ -8,5 +8,19 @@ router.get("/register", (req, res, next) => {
       <input type="text" name="House name" placeholder="Enter House name" required />
       <button type="submit">Register</button>
     </form>`);
-  next();
+  //   next();
 });
+
+router.post("/register", (req, res, next) => {
+  console.log(
+    "Handling /register for post request",
+    req.url,
+    req.method,
+    req.body
+  );
+  res.send(`<h1>Registration Successful</h1>
+    <p>Welcome, ${req.body["House name"]}!</p>`);
+  //   next();
+});
+
+module.exports = router;
--- backend/module-4/routes/userRoutes.js
+++ backend/module-4/routes/userRoutes.js
@@ -5,7 +5,7 @@ router.get("/", (req, res, next) => {
   console.log("Handling / for get request", req.url, req.method);
   res.send(`Handling / for get request
     <a href="/register"><button>Register</button></a>`);
-  next();
+  // next();
 });
 
 module.exports = router;
