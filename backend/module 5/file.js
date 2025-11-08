cfa3626160cd94a897bec65e8b54664d88380ee5
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-11-08 16:17:19 +0530

    understand the process of blocking and non blocking I/O

--- backend/module 5/file.js
+++ backend/module 5/file.js
@@ -1,38 +1,15 @@
-const { error } = require("console");
+// const { error } = require("console");
 const fs = require("fs");
 
-// // async file creation
-// fs.writeFileSync("./test.txt", "This is a test file", (err) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("File created successfully");
-//   }
-// });
-
-// // sync file creation
-// fs.readFile("./test.txt", "utf-8", (err, data) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log(data);
-//   }
-// });
-
-// fs.appendFileSync("./test.txt", `Hhello world\n`);
-
-fs.rename("./renamed_test.txt", "./test.txt", (err) => {
+console.log("start file reading");
+// sync file creation
+fs.readFile("./test.txt", "utf-8", (err, data) => {
   if (err) {
     console.log(err);
   } else {
-    console.log("File renamed successfully");
+    console.log(data);
   }
 });
+console.log("file reading in progress...");
 
-fs.unlink("./test.txt", (error) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("File deleted successfully");
-  }
-});
+console.log("end file reading");
