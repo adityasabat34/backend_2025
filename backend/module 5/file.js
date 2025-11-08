4ec1e212138ddc82afc035a1c31c32dc3d99d525
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-11-08 13:12:29 +0530

    delete file and renaming file in the node js

--- backend/module 5/file.js
+++ backend/module 5/file.js
@@ -1,21 +1,38 @@
+const { error } = require("console");
 const fs = require("fs");
 
-// async file creation
-fs.writeFileSync("./test.txt", "This is a test file", (err) => {
+// // async file creation
+// fs.writeFileSync("./test.txt", "This is a test file", (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("File created successfully");
+//   }
+// });
+
+// // sync file creation
+// fs.readFile("./test.txt", "utf-8", (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(data);
+//   }
+// });
+
+// fs.appendFileSync("./test.txt", `Hhello world\n`);
+
+fs.rename("./renamed_test.txt", "./test.txt", (err) => {
   if (err) {
     console.log(err);
   } else {
-    console.log("File created successfully");
+    console.log("File renamed successfully");
   }
 });
 
-// sync file creation
-fs.readFile("./test.txt", "utf-8", (err, data) => {
-  if (err) {
-    console.log(err);
+fs.unlink("./test.txt", (error) => {
+  if (error) {
+    console.log(error);
   } else {
-    console.log(data);
+    console.log("File deleted successfully");
   }
 });
-
-fs.appendFileSync("./test.txt", `Hhello world\n`);
