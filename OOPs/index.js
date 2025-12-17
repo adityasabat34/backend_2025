13e3aa8ff41bede92764c2330502d52043c71bb9
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-12-17 07:05:57 +0530

    understandigng into deep concept of OOPs in Js

--- OOPs/index.js
+++ OOPs/index.js
@@ -1,16 +1,31 @@
-const user = {
-  name: "Aditya",
-  age: 22,
-  lastName: "Sabat",
-  getFullName: function () {
-    return this.name + " " + this.lastName;
-  },
+// const user = {
+//   name: "Aditya",
+//   age: 22,
+//   lastName: "Sabat",
+//   getFullName: function () {
+//     return this.name + " " + this.lastName;
+//   },
 
-  getUserAge: function () {
-    return new Date().getFullYear() - user.age;
-    // console.log(this.age);
-  },
-};
+//   getUserAge: function () {
+//     return new Date().getFullYear() - user.age;
+//     // console.log(this.age);
+//   },
+// };
 
-console.log(user.getFullName());
-console.log(user.getUserAge());
+// console.log(user.getFullName());
+// console.log(user.getUserAge());
+
+function createUser(firstName, lastName, age) {
+  const user = {
+    firstName: firstName,
+    lastName: lastName,
+    age: age,
+  };
+
+  return user;
+}
+
+const user1 = createUser("Aditya", "Sabat", 22);
+const user2 = createUser("John", "Doe", 30);
+console.log(user1);
+console.log(user2);
